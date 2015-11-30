c63dd6b9e1dae607ad37653cf476d6392e68a4f3
Jared Bilton <cantops@jareds-mbp.attlocal.net>
2015-11-29 20:53:52 -0500

    added like service and modified some lits and details

---
+++ likes/likes.service.js
@@ -0,0 +1,47 @@
+(function() {
+  'use strict';
+
+  angular
+  .module('likes')
+  .factory('LikesService', function ($http, $rootScope) {
+    var url = 'https://tiny-tiny.herokuapp.com/collections/gigbag';
+    var getLikedItem = function () {
+      return $http.get(url);
+    };
+
+    var addLikedItem = function (item) {
+      $http.post(url, photo).success(function (response) {
+        console.log(response);
+        $rootScope.$broadcast('like:added');
+      });
+    };
+
+    var getOneItem = function (itemId) {
+      return $http.get(url + '/' + itemId);
+    };
+
+    var removeLikedItem = function (itemId) {
+      $http.delete(url + '/' + itemId).then(function (response) {
+        console.log(response);
+        $rootScope.$broadcast('like:deleted');
+      });
+    };
+
+    var updateLikedItem = function (item) {
+      $http.put(url + '/' + item._id, item).then(function (response) {
+        console.log(response);
+      });
+    };
+
+    return {
+      getLikes: getLikedItem,
+      getOneItem: getOneItem,
+      removeItem: removeLikedItem,
+      updateLikedItem: updateLikedItem
+    };
+
+
+
+  })
+
+}());
---
+++ shop/shop.service.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+    angular
+    .module('gigBag')
+    .factory('', function (argument) {
+      var urlOpts = {
+        baseUrl: '',
+        apiKey: 'f149a7046789c6bfb68072a6e8a9c8413be76bde344b38c3c80c7be67aadb55a',
+        method: '',
+        format: 'json',
+      }
+    })
+
+}());
